0fa5a778c61be21f9111b253899c512309eff238
IamTomide <paulinaebunoluwa@gmail.com>
2024-07-05 14:19:03 +0100

    First commit

---
+++ script.js
@@ -0,0 +1,12 @@
+
+
+new Swiper(".swiper", {
+    speed: 400,
+    spaceBetween: 100,
+    effect: "flip",
+    slidesPerView:3,
+    navigation: { 
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev"
+    }
+  });
